eb936804a49a0e0f2e9b78f462e0390aad4bcc4c
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-05-04 15:38:13 +0330

    Second commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,20 @@
 "use strict";
 
-const scorePlayerOne = document.querySelector("#score--0");
-const scorePlayerTwo = document.querySelector("#score--1");
+const scorePlayer0 = document.querySelector("#score--0");
+const scorePlayer1 = document.querySelector("#score--1");
 const dice = document.querySelector(".dice");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const currentPlayer0 = document.querySelector("#current--0");
+const currentPlayer1 = document.querySelector("#current--1");
 
-scorePlayerOne.textContent = 0;
-scorePlayerTwo.textContent = 0;
+scorePlayer0.textContent = 0;
+scorePlayer1.textContent = 0;
 dice.style.display = "none"
+let scores = [0,0]
 let dicRoll = 0
+let currentScore = 0
+let activePlayer = 0
 const randomNumber = () => {
     return Math.trunc(Math.random() * 6) + 1
 }
@@ -19,9 +23,17 @@ const rollHandler = () => {
     dice.style.display = "block"
     dicRoll = randomNumber()
     dice.src = `./assets/images/dice-${dicRoll}.png`
+    if(dicRoll !== 1){
+        currentScore += dicRoll
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore
+    }else{
+        document.querySelector(`#current--${activePlayer}`).textContent = 0
+        activePlayer = activePlayer === 0 ? 1 : 0
+        currentScore = 0
+    }
 }
 const holdHandler = () =>{
-    scorePlayerOne.textContent = dicRoll
+    scorePlayer0.textContent = dicRoll
 }
 
 
