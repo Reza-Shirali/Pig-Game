8e846508f652b59768f69539beb566a6ac856baf
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-05-04 16:33:21 +0330

    three commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,35 +7,75 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const currentPlayer0 = document.querySelector("#current--0");
 const currentPlayer1 = document.querySelector("#current--1");
+const playerEl0 = document.querySelector(".player--0");
+const playerEl1 = document.querySelector(".player--1");
+const btnNew = document.querySelector(".btn--new");
 
 scorePlayer0.textContent = 0;
 scorePlayer1.textContent = 0;
-dice.style.display = "none"
-let scores = [0,0]
-let dicRoll = 0
-let currentScore = 0
-let activePlayer = 0
+dice.style.display = "none";
+let scores = [0, 0];
+let dicRoll = 0;
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
 const randomNumber = () => {
-    return Math.trunc(Math.random() * 6) + 1
-}
+  return Math.trunc(Math.random() * 6) + 1;
+};
+const switchPlayer = () => {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  playerEl0.classList.toggle("player--active");
+  playerEl1.classList.toggle("player--active");
+};
 
 const rollHandler = () => {
-    dice.style.display = "block"
-    dicRoll = randomNumber()
-    dice.src = `./assets/images/dice-${dicRoll}.png`
-    if(dicRoll !== 1){
-        currentScore += dicRoll
-        document.querySelector(`#current--${activePlayer}`).textContent = currentScore
-    }else{
-        document.querySelector(`#current--${activePlayer}`).textContent = 0
-        activePlayer = activePlayer === 0 ? 1 : 0
-        currentScore = 0
+  if (playing) {
+    dice.style.display = "block";
+    dicRoll = randomNumber();
+    dice.src = `./assets/images/dice-${dicRoll}.png`;
+    if (dicRoll !== 1) {
+      currentScore += dicRoll;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
     }
-}
-const holdHandler = () =>{
-    scorePlayer0.textContent = dicRoll
-}
+  }
+};
+const holdHandler = () => {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+  }
 
+  if (scores[activePlayer] >= 20) {
+    playing = false;
+    dice.style.display = "none";
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+  } else {
+    switchPlayer();
+  }
+};
+const newHandler = () => {
+  //   scores = [0, 0];
+  //   scorePlayer0.textContent = 0;
+  //   scorePlayer1.textContent = 0;
+  //   currentScore = scores[activePlayer];
+  //   if (activePlayer === 0 || activePlayer === 1) {
+  //     activePlayer = 0;
+  //     playerEl0.classList.toggle("player--active");
+  //     playerEl1.classList.toggle("player--active");
+  //   }
+};
 
-btnRoll.addEventListener("click",rollHandler)
-btnHold.addEventListener("click",holdHandler)
+btnRoll.addEventListener("click", rollHandler);
+btnHold.addEventListener("click", holdHandler);
+btnNew.addEventListener("click", newHandler);
